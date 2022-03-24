154a622a9bc9afd3f37d82ad384cbccd1cba50df
Jony Vieira <98748765+DaringRX@users.noreply.github.com>
2022-03-24 17:13:03 -0300

    Add files via upload

---
+++ js/graficos.js
@@ -0,0 +1,73 @@
+
+  
+
+
+  window.onload = function(){
+      
+  const data = {
+    labels: [
+      'Refeição',
+      'Combustivel',
+      'Outros'
+    ],
+    datasets: [{
+      label: 'Gastos Diários',
+      data: [50, 150, 120],
+      backgroundColor: [
+        'rgb(255, 99, 132)',
+        'rgb(54, 162, 235)',
+        'rgb(255, 205, 86)'
+      ],
+      hoverOffset: 4
+    }]
+  };
+
+  const config = {
+    type: 'doughnut',
+    data: data,
+    options: {
+        responsive: true
+    }
+  };
+
+
+  
+  const myChart = new Chart(
+    document.getElementById('myChart'),
+    config
+  );
+
+  
+  const data2 = {
+    labels: [
+      'Refeição',
+      'Combustivel',
+      'Outros'
+    ],
+    datasets: [{
+      label: 'Gastos Mensais',
+      data: [150, 150, 20],
+      backgroundColor: [
+        'rgb(220,20,60)',
+        'rgb(70,130,180)',
+        'rgb(255,215,0)'
+      ],
+      hoverOffset: 4
+    }]
+  };
+
+  const config2 = {
+    type: 'doughnut',
+    data: data2,
+    options: {
+        responsive: true
+    }
+  };
+
+
+  
+  const myChart2 = new Chart(
+    document.getElementById('myChart2'),
+    config2
+  );
+  }
